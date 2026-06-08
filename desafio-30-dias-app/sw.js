15f06df084b39ca9317198f8d0259dc994ce9d42
will1997ti <nwstore.contato1@gmail.com>
2026-06-07 21:09:40 -0300

    Bump desafio app cache version

--- desafio-30-dias-app/sw.js
+++ desafio-30-dias-app/sw.js
@@ -1,10 +1,19 @@
-const CACHE_NAME = 'desafio-30dias-v1';
+const CACHE_NAME = 'desafio-30dias-v3';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
 });
